4763435e0223cd0bf24eb1bf893d01ebd8380a14
Mohanvijay8344 <Mohanvijay8344@gmail.com>
2023-11-23 20:21:51 +0530

    resume update

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function Navbar() {
                 </Link>
                 <a
                   className="text-gray-300 hover-bg-gray-700 hover-text-white px-3 py-2 rounded-md text-sm font-medium"
-                  href="https://drive.google.com/file/d/1bd5jVEV5Q6Ilj1_e7tW__Vpq2Efum5pt/view"
+                  href="https://drive.google.com/file/d/1dpLeAXYOOe6KOd78kt4NDXJXoEz0Y4Hk/view"
                   target="_blank"
                 >
                   Resume
