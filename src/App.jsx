f301eabd506fe83f35688e8555cd9c75f119b400
Mohanvijay8344 <Mohanvjay8344@gmail.com>
2024-01-21 14:05:10 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,9 @@ export function Navbar()  {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 flex gap-2 flex-ro">
               <img className="h-8 w-8 logo" src={logo} alt="Logo" />
+              <span className="text-yellow-600">Mohanraj</span>
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
